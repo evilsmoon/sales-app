5df61dd6ac8faa9333febd6f2f9c3a225ac61a1b
evilsmoon <gabrielmorales201564@gmail.com>
2022-01-21 12:02:40 -0500

    dio mio

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
-var http = require('http');
-var server = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    var message = 'It works!\n',
-        version = 'NodeJS ' + process.versions.node + '\n',
-        response = [message, version].join('\n');
-    res.end(response);
+const express = require('express');
+const app = express();
+
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
+
+
+app.use(express.static(__dirname + '/public'));
+
+// Express HBS engine
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
+
+
+
+
+app.get('/', (req, res) => {
+
+    res.render('home', {
+        nombre: 'Dieggo :3 '
+    });
 });
-server.listen();
+
+app.get('/about', (req, res) => {
+
+    res.render('about');
+});
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${ port }`);
+});
